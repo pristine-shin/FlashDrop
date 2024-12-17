de3bf5e8f4baf7fba7dab39cf2d780007f329e01
Pristine Shin <prisvsyo@gmail.com>
2024-12-16 18:48:58 -0800

    user profile page styled, edited nav and footer css

--- react-vite/src/components/UserProfilePage/UserProfile.jsx
+++ react-vite/src/components/UserProfilePage/UserProfile.jsx
@@ -76,7 +76,7 @@ const ProfilePage = () => {
         const now = new Date();
         const diffInTime = now - createdDate;
         return Math.floor(diffInTime / (1000 * 3600 * 24));
-      };
+    };
 
     if (error) return <p>{error}</p>;
     if (!user) return (
@@ -91,34 +91,44 @@ const ProfilePage = () => {
 
 
     return (
-        <div className="all-post-row">
-            <div className="profile-header">
-                <img src={user.profileImageUrl} alt="profile pic" id="big-profile-pic"/>
-                <h2 className="all-posts-heading">@{user.username}</h2>
-            </div>
-            <div className="all-posts">
-                {user.posts && user.posts.length > 0 ? (
-                    user.posts.map((post) => (
-                        <div key={post.id} className="all-post-card">
-                            <Link to={`/posts/${post.id}`} className="all-post-card-link">
-                                <div className="all-post-header">
-                                    <h3 className="all-post-username">{post.username}</h3>
-                                </div>
-                                <img src={post.imageUrl} alt="post image" className="all-post-image" />
-                                <div className="all-post-info">
-                                    <p className="all-post-price">${post.price}</p>
-                                    <p className="all-post-style">{post.style}</p>
-                                    <p className="all-post-size">{post.size}</p>
-                                    <p className="all-post-available">{post.available}</p>
-                                    <p className="all-post-caption">{post.caption}</p>
-                                    <p className="all-post-createdAt">{calculateDaysAgo(post.createdAt)} days ago</p>
-                                </div>
-                            </Link>
-                        </div>
-                    ))
-                ) : (
-                    <p>No posts available.</p>
-                )}
+        <div className="profile-page-container">
+            <div className="all-post-row">
+                <div className="profile-header">
+                    <div className="profile-header-row-1">
+                        <img src={user.profileImageUrl} alt="profile pic" id="big-profile-pic" />
+                        <div className="profile-header-details">{user.posts.length} posts</div>
+                        <div className="profile-header-details">121 followers</div>
+                        <div className="profile-header-details">33 follows</div>
+                    </div>
+                    <div className="profile-header-row-2">
+                        <h2 className="profile-username">{user.username}</h2>
+                        <h3 className="profile-bio">{user.bio}</h3>
+                    </div>
+                </div>
+                <div className="all-posts">
+                    {user.posts && user.posts.length > 0 ? (
+                        user.posts.map((post) => (
+                            <div key={post.id} className="all-post-card">
+                                <Link to={`/posts/${post.id}`} className="all-post-card-link">
+                                    <div className="all-post-header">
+                                        <h3 className="all-post-username">{post.username}</h3>
+                                    </div>
+                                    <img src={post.imageUrl} alt="post image" className="all-post-image" />
+                                    <div className="all-post-info">
+                                        <p className="all-post-price">${post.price}</p>
+                                        <p className="all-post-style">{post.style}</p>
+                                        <p className="all-post-size">{post.size}</p>
+                                        <p className="all-post-available">{post.available}</p>
+                                        <p className="all-post-caption">{post.caption}</p>
+                                        <p className="all-post-createdAt">{calculateDaysAgo(post.createdAt)} days ago</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No posts available.</p>
+                    )}
+                </div>
             </div>
         </div>
     );
