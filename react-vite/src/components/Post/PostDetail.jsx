69e50e4f6bc72b75a691e537827a1c699445b6ae
Pristine Shin <prisvsyo@gmail.com>
2024-12-27 15:58:55 -0800

    post detail component styled

--- react-vite/src/components/Post/PostDetail.jsx
+++ react-vite/src/components/Post/PostDetail.jsx
@@ -136,22 +136,22 @@ const PostDetail = () => {
           <div className="post-column">
             {/* <div className="post-meta"> */}
 
-              <div className="post-image-column">
-                <img
-                  src={post.imageUrl}
-                  alt="post image"
-                  className="post-image-big"
-                />
-                <button
-                  className="post-detail-button"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => openModal("/images/meme.jpg")}
-                >
-                  <FontAwesomeIcon icon={faShare} className="nav-icon" /> Share
-                  / Embed
-                </button>
-                {/* Favorites Button */}
-                {/* {sessionUser &&
+            <div className="post-image-column">
+              <img
+                src={post.imageUrl}
+                alt="post image"
+                className="post-image-big"
+              />
+              <button
+                className="post-detail-button"
+                style={{ cursor: "pointer" }}
+                onClick={() => openModal("https://res.cloudinary.com/dmvfvyilq/image/upload/v1735340873/modern-coming-soon-loading-icon-600nw-2506897855_zdkeal.jpg")}
+              >
+                <FontAwesomeIcon icon={faShare} className="nav-icon" /> Share
+                / Embed
+              </button>
+              {/* Favorites Button */}
+              {/* {sessionUser &&
                   (favorites[postId] ?
                     <button
                       onClick={() => removeFromFavorites(postId)}
@@ -170,79 +170,85 @@ const PostDetail = () => {
                       Favorites
                     </button>
                   )} */}
-                {/* Comments Section */}
-                <div className="comments-section">
-                  <p className="comments-title">Comments</p>
-                  {comments.length > 0 ? (
-                    <>
-                      {comments.map((comment, index) => (
-                        <div className="comment" key={index}>
-                          <div className="comment-image">
-                            <img
-                              src={comment.profileImageUrl}
-                              alt={`${comment.username}'s profile pic`}
-                              className="profile-image"
-                            />
-                          </div>
-                          <div className="comment-info">
-                            <p className="comment-content">
-                              <span className="comment-name">
-                                <strong>
-                                  {comment.username || "Anonymous"}
-                                </strong>
-                              </span>
-                              {comment.content}
-                            </p>
-                            {sessionUser && sessionUser.id === comment.userId ? (
-                              <>
-                                <button
-                                  onClick={() => openEditCommentModal(comment)}
-                                  className="post-detail-button"
-                                >
-                                  <FontAwesomeIcon
-                                    icon={faPenToSquare}
-                                    className="nav-icon"
-                                  />
-                                  Edit
-                                </button>
-                                <button
-                                  onClick={() => openRemoveCommentModal(comment)}
-                                  className="post-detail-button"
-                                >
-                                  <FontAwesomeIcon
-                                    icon={faTrash}
-                                    className="nav-icon"
-                                  />
-                                  Remove
-                                </button>
-                              </>
-                            ) : (
-                              <div></div>
-                            )}
-                          </div>
-                        </div>
-                      ))}
-                    </>
-                  ) : (
-                    <p>No comments available for this post.</p>
-                  )}
-                  {sessionUser &&
-                    post.userId !== sessionUser.id &&
-                    !comments.find(
-                      (comment) => comment.userId === sessionUser.id
-                    ) ? (
-                    <button
-                      onClick={openAddCommentModal}
-                      className="post-detail-button"
-                    >
-                      <FontAwesomeIcon icon={faPlus} className="nav-icon" />
-                      Add
-                    </button>
-                  ) : (
-                    <div></div>
-                  )}
-                </div>
+              <div className="post-artist-name">
+                @{post.username}
               </div>
+              <div>
+                {post.caption}
+              </div>
+              {/* Comments Section */}
+              <div className="comments-section">
+                <p className="comments-title">Comments</p>
+                {comments.length > 0 ? (
+                  <>
+                    {comments.map((comment, index) => (
+                      <div className="comment" key={index}>
+                        <div className="comment-image">
+                          <img
+                            src={comment.profileImageUrl}
+                            alt={`${comment.username}'s profile pic`}
+                            className="profile-image"
+                          />
+                        </div>
+                        <div className="comment-info">
+                          <p className="comment-content">
+                            <span className="comment-name">
+                              <strong>
+                                {comment.username || "Anonymous"}
+                              </strong>
+                            </span>
+                            {comment.content}
+                          </p>
+                          {sessionUser && sessionUser.id === comment.userId ? (
+                            <>
+                              <button
+                                onClick={() => openEditCommentModal(comment)}
+                                className="post-detail-button"
+                              >
+                                <FontAwesomeIcon
+                                  icon={faPenToSquare}
+                                  className="nav-icon"
+                                />
+                                Edit
+                              </button>
+                              <button
+                                onClick={() => openRemoveCommentModal(comment)}
+                                className="post-detail-button"
+                              >
+                                <FontAwesomeIcon
+                                  icon={faTrash}
+                                  className="nav-icon"
+                                />
+                                Remove
+                              </button>
+                            </>
+                          ) : (
+                            <div></div>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                ) : (
+                  <p>No comments available for this post.</p>
+                )}
+                {sessionUser &&
+                  post.userId !== sessionUser.id &&
+                  !comments.find(
+                    (comment) => comment.userId === sessionUser.id
+                  ) ? (
+                  <button
+                    onClick={openAddCommentModal}
+                    className="post-detail-button"
+                  >
+                    <FontAwesomeIcon icon={faPlus} className="nav-icon" />
+                    Add
+                  </button>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
             {/* </div> */}
           </div>
           <div className="artist-column">
@@ -252,26 +258,26 @@ const PostDetail = () => {
               alt={`${post.username}'s profile`}
               className="profile-image-small"
             />
-            <p className="post-artist">{post.username}</p>
+            <p className="post-artist">@{post.username}</p>
             <button
               className="follow"
               style={{ cursor: "pointer" }}
-              onClick={() => openModal("/images/meme.jpg")}
+              onClick={() => openModal("https://res.cloudinary.com/dmvfvyilq/image/upload/v1735340873/modern-coming-soon-loading-icon-600nw-2506897855_zdkeal.jpg")}
             >
               Follow
             </button>
-            <p className="artist-bio">
+            <p className="detail-artist-bio">
               {post.artistBio}<br></br>
               <br></br>
             </p>
             <p className="post-created-time">
               {formatDate(post.createdAt)}
             </p>
-            <p className="artist-contact">contact / help</p>
+            <p className="artist-contact">Contact / Help</p>
             <p
               className="bio-more"
               style={{ cursor: "pointer" }}
-              onClick={() => openModal("/images/meme.jpg")}
+              onClick={() => openModal("https://res.cloudinary.com/dmvfvyilq/image/upload/v1735340873/modern-coming-soon-loading-icon-600nw-2506897855_zdkeal.jpg")}
             >
               <br></br>Contact {post.username}
               <br></br>
